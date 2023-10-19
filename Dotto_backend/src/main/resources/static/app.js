b44f417278e0cfcdc7cea35a54e67a0ccaad9e03
Sim JuHeun <88835746+tejava7177@users.noreply.github.com>
2023-10-19 20:48:45 +0900

    Add files via upload

--- Dotto_backend/src/main/resources/static/app.js
+++ Dotto_backend/src/main/resources/static/app.js
@@ -1,31 +1,41 @@
+
 const btn = document.querySelector("#countbtn");
 const clickCount = document.querySelector("#countNum");
-
-
-
-console.log(btn);
-var count = 0;
+const fill = document.getElementById("fill");
+const maxCount = 30000; // 최대 값
+let count = parseInt(localStorage.getItem('json')) || 0;
 
 function increase() {
- // 로컬 스토리지에서 저장된 값을 불러옴
-    var count = parseInt(localStorage.getItem('json')) || 0;
-
-    count = count + 1;
-    clickCount.innerText = count;
-    localStorage.setItem('json', JSON.stringify(count));
-
-     // 서버로 업데이트된 값을 보내는 AJAX 요청
-        fetch("/updateCount?count=" + count, {
-            method: "POST"
-        });
+    if (count < maxCount) {
+        count += 1;
+        updateCountDisplay();
+        localStorage.setItem('json', JSON.stringify(count));
+    }
 }
 
+function reset() {
+    count = 0;
+    updateCountDisplay();
+    console.log(count);
+    localStorage.setItem('json', '0'); // 로컬 스토리지 값을 0으로 초기화
+}
 
+function updateCountDisplay() {
+    clickCount.textContent = count;
+    const fillPercentage = (count / maxCount) * 100;
+    countNum.textContent = `${count} / ${maxCount} 원`; // count 값 / 30,000 형식으로 표시
+    fill.style.width = `${fillPercentage}%`;
+}
+// 초기 로딩 시 업데이트
+updateCountDisplay();
+//btn.addEventListener("click", increase);
 
+document.querySelector("#resetbtn").addEventListener("click", reset); // 초기화 버튼에 이벤트 핸들러 추가
+document.getElementById("dotto").addEventListener("click", increase); // 이미지 클릭 이벤트 핸들러 추가
 
 $(function () {
-    $('button').toggle(
-        function () { //버튼 클릭할때 마다 이미지 바뀜                
+    $('#dotto').toggle(
+        function () {
             $('#dotto').attr('src', 'dotto_nuki2.png');
         },
         function () {
@@ -33,9 +43,13 @@ $(function () {
         });
 });
 
+const tooltipIcon = document.querySelector('.tooltip-icon');
+const tooltipText = document.querySelector('.tooltip-text');
 
-btn.addEventListener("click", increase);
+tooltipIcon.addEventListener('mouseenter', () => {
+    tooltipText.style.display = 'block';
+});
 
-
-
-
+tooltipIcon.addEventListener('mouseleave', () => {
+    tooltipText.style.display = 'none';
+});
