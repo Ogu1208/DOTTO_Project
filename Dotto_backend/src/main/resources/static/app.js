4b834c9253fe1f5e443215c4b08dc2d215f30c1f
tejava7177 <wngmstla@sch.ac.kr>
2023-10-19 23:21:03 +0900

    [feat] 키보드 입력 시에도 숫자 증가 추가

--- Dotto_backend/src/main/resources/static/app.js
+++ Dotto_backend/src/main/resources/static/app.js
@@ -5,13 +5,22 @@ const fill = document.getElementById("fill");
 const maxCount = 30000; // 최대 값
 let count = parseInt(localStorage.getItem('json')) || 0;
 
+
+const dotto = $('#dotto');
+let isImageNuki = true;
+
+// 포커스 주기
+document.body.focus();
+
 function increase() {
     if (count < maxCount) {
         count += 1;
         updateCountDisplay();
         localStorage.setItem('json', JSON.stringify(count));
+        //toggleImage();
     }
 }
+
 
 function reset() {
     count = 0;
@@ -26,12 +35,60 @@ function updateCountDisplay() {
     countNum.textContent = `${count} / ${maxCount} 원`; // count 값 / 30,000 형식으로 표시
     fill.style.width = `${fillPercentage}%`;
 }
-// 초기 로딩 시 업데이트
-updateCountDisplay();
-//btn.addEventListener("click", increase);
+
+
 
 document.querySelector("#resetbtn").addEventListener("click", reset); // 초기화 버튼에 이벤트 핸들러 추가
-document.getElementById("dotto").addEventListener("click", increase); // 이미지 클릭 이벤트 핸들러 추가
+// 이미지 클릭 이벤트 핸들러 추가
+document.getElementById("dotto").addEventListener("click", function () {
+    increase();
+    toggleImage();
+});
+
+// 키보드 입력 이벤트 핸들러 추가
+document.body.addEventListener("keydown", function (event) {
+    if (count < maxCount) {
+        increase();
+        toggleImage();
+    }
+});
+
+
+
+
+
+// 이미지 전환 함수
+//function toggleImage() {
+////    $('#dotto').toggle(
+////        function () {
+////            $('#dotto').attr('src', 'dotto_nuki2.png');
+////        },
+////        function () {
+////            $('#dotto').attr('src', 'dotto_nuki.png');
+////        });
+//    const dotto = $('#dotto');
+//        if (dotto.attr('src') === 'dotto_nuki.png') {
+//            dotto.attr('src', 'dotto_nuki2.png');
+//        } else {
+//            dotto.attr('src', 'dotto_nuki.png');
+//        }
+//
+//}
+function toggleImage() {
+    if (isImageNuki) {
+        dotto.attr('src', 'dotto_nuki2.png');
+    } else {
+        dotto.attr('src', 'dotto_nuki.png');
+    }
+    isImageNuki = !isImageNuki;
+}
+
+
+
+//btn.addEventListener("click", increase);
+// 초기 로딩 시 업데이트
+updateCountDisplay();
+
 
 $(function () {
     $('#dotto').toggle(
