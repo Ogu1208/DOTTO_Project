a8ea6e4335bb5a272330956045b1ebb687b27615
Sim-JuHeun <juheun9912@gmail.com>
2025-09-04 16:46:24 +0900

    백엔드 기본 설정

---
+++ dotto_backend/src/main/java/com/dotto_backend/DottoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.dotto_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DottoBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DottoBackendApplication.class, args);
+    }
+
+}
---
+++ dotto_backend/src/test/java/com/dotto_backend/DottoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dotto_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DottoBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
